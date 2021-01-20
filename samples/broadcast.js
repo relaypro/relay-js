f852373777150dd860bda36443326dcafc955949
Brandon Smith <bsmith@republicwireless.com>
2021-01-20 15:50:05 -0500

    always terminate broadcast workflow after sending notification regardless of ack requirement

--- samples/broadcast.js
+++ samples/broadcast.js
@@ -9,24 +9,8 @@ const createApp = (relay) => {
       await relay.alert(BROADCAST_NAME, text, actualTargets)
     } else {
       await relay.broadcast(text, actualTargets)
-      await relay.terminate()
     }
-  })
-
-  relay.on(`button`, async () => {
-    console.log(`action button pressed while outstanding acks exist`)
-    await relay.say(`Not all broadcasts have been acknowledged yet.`)
-  })
-
-  relay.on(`notification`, async (source, id, event, state) => {
-    console.log(`notification state is ${state}`)
-    const { acknowledged, created } = state
-    if (acknowledged) {
-      console.log(`all braodcast notifications have been acked, shutting down`)
-      relay.terminate()
-    } else {
-      console.log(`${source} has acked the broadcast, ${created?.length - acknowledged?.length} left to ack of ${created?.length}`)
-    }
+    await relay.terminate()
   })
 }
 
