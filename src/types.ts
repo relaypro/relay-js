8f7d65d976d1f7b5c2bf074d43e8f9647cd93f01
Brandon Smith <bsmith@republicwireless.com>
2021-04-28 17:12:01 -0400

    More generic LED action
    * `ledAction(effect, args)`
    * all existing led methods utilize ledAction
    * Typings for effect and args

--- src/types.ts
+++ src/types.ts
@@ -6,6 +6,17 @@ import { Button, IncidentStatus, NotificationPriority, NotificationSound, Taps }
 import { RelayEventAdapter } from './index'
 
 export type LedIndex = `ring`|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|`1`|`2`|`3`|`4`|`5`|`6`|`7`|`8`|`9`|`10`|`11`|`12`|`13`|`14`|`15`|`16`
+export type LedEffect = `off` | `breathe`| `flash` | `rotate` | `rainbow` | `static`
+export type LedInfo = {
+  rotations?: number,
+  count?: number,
+  duration?: number,
+  repeat_delay?: number,
+  pattern_repeats?: number,
+  colors?: {
+    [K in LedIndex]?: string
+  }
+}
 
 export interface Msg {
   _id: string,
